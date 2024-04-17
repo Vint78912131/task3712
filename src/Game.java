e5cef64d3049c5a08851ffa5b7f0266ce72dae3c
v sotnikov <sotnikov@rosplatforma.ru>
2024-04-17 14:04:46 +0300

    17042024commit

---
+++ src/Game.java
@@ -0,0 +1,14 @@
+public abstract class Game {
+    public abstract void prepareForTheGame();
+
+    public abstract void playGame();
+
+    public abstract void congratulateWinner();
+
+    public void run() {
+        prepareForTheGame();
+        playGame();
+        congratulateWinner();
+    }
+}
+
